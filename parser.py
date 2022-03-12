43028f3548b4e3de9f4a357d4883a6cd6ceb8f60
BetrsoA <bbabraha@ucsc.edu>
2022-03-12 08:34:09 -0800

    Update parser.py

--- parser.py
+++ parser.py
@@ -7,12 +7,11 @@ from time import sleep
 url = 'https://api.pushshift.io/reddit/search/submission/'
 
 def get_pushshift_data(after, before, sub, keyword):
-  # ?&after=' + \
-  #     str(after)+'&before='+str(before) + \
-  #     '&subreddit='+str(sub)+'&q='+str(keyword)
 
   print(url)
-  request = requests.get(url, params={"after": after, "before": before, "subreddit": sub, "q": keyword, "size": 500})
+  
+  request = requests.get(url, params={"after": after, "before": before, "subreddit": sub, "q": keyword, "size": 100})
+  
   return request.json()
 
 
@@ -40,6 +39,9 @@ timeEnd = int(timeEnd)
 sub = "uberdrivers" #input("What subreddit would you like to seach in?: ")
 keyword = "depressed|depression|depressing" #input("What keyword are you searching for?: ")
 
+# posts is a list of dictionaries, where each dictionary represents one post
+# this contains all the data we got from the reddit API about the post with out comments
+# posts are called submissions on the API
 posts = get_pushshift_data(timeStart, timeEnd, sub, keyword)["data"]
 
 entries = []
@@ -51,13 +53,25 @@ for post in posts:
   
   request_start = datetime.datetime.now()
   try:
+    # the original request only got the  posts and not their comments, so,
+    # for each of the posts, we do another request that fetches the 
+    # comments descending from that specific post (including comments-on-comments, because that's what "link_id" gives you)
     request = requests.get('https://api.pushshift.io/reddit/search/comment/',
-                          params={"link_id": post["id"], "sort": "score:desc", "size": 500})
+                          params={"link_id": post["id"], "sort": "score:desc", "size": 100})
     replies = request.json()["data"]
   except Exception as e:
     print(f"Request exception: {e}")
     replies = []
-    
+  # Currently, it seems that Reddit rejects requests if you make more than 60 
+  # requests in one minute. So to prevent that from happening, we want to
+  # wait AT LEAST one second between starting each request.
+  # datetime.datetime.now() gets you the current time, as a "datetime" object
+  # when you subtract two of these, you get a "timedelta" object
+  # on the "timedelta" object, you can call the method "total_seconds"
+  # which converts it from a fancy timedelta object into a plain number of seconds.
+  # That gets us the amount of time since the request started, in seconds;
+  # we then want to wait for however much time is LEFT to pad it up to one second.
+
   elapsed = (datetime.datetime.now() - request_start).total_seconds()
   if elapsed < 1:
         sleep(1.01 - elapsed)
@@ -70,11 +84,19 @@ for entry in entries:
 
 desired_columns = ["id","author", "title", "subreddit","num_comments", "score", "selftext", "body", "permalink"]
 
+# from_records makes a new dataframe object, with columns determined by whatever is in "entries"
 whole_df = pd.DataFrame.from_records(entries)
+
+# we want to have desired_columns, but some of them might be missing (because, for example,
+# there might not have been any comments, so none of the entries would have contained "body",
+# so Pandas wouldn't have generated a "body" column). So, if any were missing, explicitly add 
+# the missing ones in here:
+
 for column_name in desired_columns:
       if column_name not in whole_df:
             whole_df[column_name] = ""
 
+# make a new copy of the dataframe with only the desired columns in it (leaving out the others)
 specific_columns_df = whole_df[desired_columns]
 
 specific_columns_df.to_csv("reddit_data.csv")
